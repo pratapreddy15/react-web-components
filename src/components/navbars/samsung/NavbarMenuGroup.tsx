320aa2b2374f33bbe5df356351b04d348b191bdb
Pratap Reddy <pratapreddy15@gmail.com>
2024-07-29 20:42:30 +1000

    removing navbargroupmenu

--- src/components/navbars/samsung/NavbarMenuGroup.tsx
+++
@@ -1,9 +0,0 @@
-import classes from './NavbarMenuGroup.module.css';
-
-interface NavbarMenuGroupProps {
-  children: React.ReactNode;
-}
-
-export function NavbarMenuGroup({ children }: NavbarMenuGroupProps) {
-  return <ul className={classes.navbarMenuGroup}>{children}</ul>;
-}
